f9a552be76d5ef1d719131d798db1d3a2e78e936
Matthias Geörg <mail@macbarfuss.de>
2011-08-18 20:32:45 +0200

    Initial import.

---
+++ magnifood/target/generated-sources/gwt/de/macbarfuss/client/Messages.java
@@ -0,0 +1,26 @@
+package de.macbarfuss.client;
+
+/**
+ * Interface to represent the messages contained in resource bundle:
+ * 	/home/macbarfuss/Development/workspace-indigo/magnifood/src/main/resources/de/macbarfuss/client/Messages.properties'.
+ */
+public interface Messages extends com.google.gwt.i18n.client.Messages {
+  
+  /**
+   * Translated "Enter your name".
+   * 
+   * @return translated "Enter your name"
+   */
+  @DefaultMessage("Enter your name")
+  @Key("nameField")
+  String nameField();
+
+  /**
+   * Translated "Send".
+   * 
+   * @return translated "Send"
+   */
+  @DefaultMessage("Send")
+  @Key("sendButton")
+  String sendButton();
+}
